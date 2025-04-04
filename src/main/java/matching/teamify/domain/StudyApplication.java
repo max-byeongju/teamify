1d3f0943830973c2b8c473cfb9e6976da1327fa5
max-byeongju <ifconfig@g.hongik.ac.kr>
2025-04-05 00:07:07 +0900

    feat: StudyApplication Entity 추가

---
+++ src/main/java/matching/teamify/domain/StudyApplication.java
@@ -0,0 +1,29 @@
+package matching.teamify.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class StudyApplication {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    private Study study;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public StudyApplication(Study study, Member member) {
+        this.study = study;
+        this.member = member;
+    }
+
+}

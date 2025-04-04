f64228c5e19f933e5c5bac392c5b68d2a6046ebf
max-byeongju <ifconfig@g.hongik.ac.kr>
2025-04-05 00:10:59 +0900

    feat: FavoriteStudy Entity 추가

---
+++ src/main/java/matching/teamify/domain/FavoriteStudy.java
@@ -0,0 +1,29 @@
+package matching.teamify.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class FavoriteStudy {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    private Study study;
+
+    public FavoriteStudy(Member member, Study study) {
+        this.member = member;
+        this.study = study;
+    }
+
+}

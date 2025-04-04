2342001c2d4057846df94df6bf677e142ac5ce73
max-byeongju <ifconfig@g.hongik.ac.kr>
2025-04-05 00:09:11 +0900

    feat: FavoriteProject Entity 추가

---
+++ src/main/java/matching/teamify/domain/FavoriteProject.java
@@ -0,0 +1,29 @@
+package matching.teamify.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class FavoriteProject {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    public FavoriteProject(Member member, Project project) {
+        this.member = member;
+        this.project = project;
+    }
+
+}

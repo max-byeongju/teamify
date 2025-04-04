3196b08006dcc49a1e3a694f87b89465c4906866
max-byeongju <ifconfig@g.hongik.ac.kr>
2025-04-04 17:26:04 +0900

    initial setting

---
+++ src/main/java/matching/teamify/TeamifyApplication.java
@@ -0,0 +1,13 @@
+package matching.teamify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matching/teamify/TeamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package matching.teamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

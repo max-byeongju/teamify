3b7ecd2865d1df37804771f616063b98afc4103d
max-byeongju <ifconfig@g.hongik.ac.kr>
2025-04-05 22:56:18 +0900

    feat: 회원가입 시 중복 ID 검사

--- src/main/java/matching/teamify/repository/MemberRepository.java
+++ src/main/java/matching/teamify/repository/MemberRepository.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import matching.teamify.domain.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
@@ -13,6 +16,13 @@ public class MemberRepository {
 
     public void save(Member member) {
         em.persist(member);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+        return em.createQuery("select m from Member m where m.loginId = :loginId", Member.class)
+                .setParameter("loginId", loginId)
+                .getResultStream()
+                .findFirst();
     }
 
 }
--- src/main/java/matching/teamify/service/MemberService.java
+++ src/main/java/matching/teamify/service/MemberService.java
@@ -6,6 +6,9 @@ import matching.teamify.dto.member.MemberSignupRequest;
 import matching.teamify.repository.MemberRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +17,15 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void createMember(MemberSignupRequest requestDto) {
+
+        Optional<Member> byLoginId = memberRepository.findByLoginId(requestDto.getLoginId());
+
+        if (byLoginId.isPresent()) {
+            throw new RuntimeException("이미 사용중인 ID 입니다.");
+        }
+
         String pw = passwordEncoder.encode(requestDto.getPassword());
 
         Member member = Member.builder()

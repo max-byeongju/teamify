04b69f164955f663fd26b8f55f44539ec8df0300
max-byeongju <ifconfig@g.hongik.ac.kr>
2025-04-06 03:31:47 +0900

    feat: LoginInCheckterceptor 구현

--- src/main/java/matching/teamify/config/WebConfig.java
+++ src/main/java/matching/teamify/config/WebConfig.java
@@ -1,11 +1,21 @@
 package matching.teamify.config;
 
+import matching.teamify.config.auth.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/login", "/auth/logout", "/members");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {

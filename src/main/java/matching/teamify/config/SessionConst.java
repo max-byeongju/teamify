c29f61fdb0efc8971ec77195fdef554988f6b5d0
max-byeongju <ifconfig@g.hongik.ac.kr>
2025-04-05 23:50:47 +0900

    feat: 로그인/로그아웃 기능 구현

---
+++ src/main/java/matching/teamify/config/SessionConst.java
@@ -0,0 +1,5 @@
+package matching.teamify.config;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
---
+++ src/main/java/matching/teamify/service/LoginService.java
@@ -0,0 +1,29 @@
+package matching.teamify.service;
+
+import lombok.RequiredArgsConstructor;
+import matching.teamify.domain.Member;
+import matching.teamify.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Member login(String loginId, String password) {
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(() -> new RuntimeException("존재하지 않는 ID 입니다."));
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new RuntimeException("비밀번호가 일치하지 않습니다");
+        }
+
+        return member;
+    }
+
+
+}
